174625694f6ab4d0844b65728767469a63e9b841
Eelco van Vliet <eelco.van-vliet@tatasteel.com>
2014-01-06 10:57:11 +0100

    update gridpro2Foam with new OpenFOAM export format

---
+++ gridpro2FOAM.hpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+static const char *PREFIX_INFO = "=====>     "; // prefix for information
+static const char *PREFIX_ACTION = " --> ";
+
